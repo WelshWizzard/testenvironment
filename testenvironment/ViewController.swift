f8b39cedc6cca05996150ffd5e5fae84aca3aadc
Shaw Goodwin <shaw.goodwin@gmail.com>
2019-07-13 15:09:27 +0100

    Test project to check environment set up correctly

---
+++ testenvironment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testenvironment
+//
+//  Created by Shaw Goodwin on 13/07/2019.
+//  Copyright © 2019 Shaw Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
